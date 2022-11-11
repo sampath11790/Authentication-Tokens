0f4cec9d80c123d0474dd6a465983ed4cab25b0e
sampath11790 <bsampath11790@gmail.com>
2022-11-11 21:00:30 +0530

    added sending information and stored user data in database and error alert added

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,28 +1,69 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
-
+  const [loading, setloading] = useState(false);
+  const enteredEmailRef = useRef();
+  const enteredPaswordRef = useRef();
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
+  };
+  const loginHandler = async (event) => {
+    setloading(true);
+    try {
+      event.preventDefault();
+      const userobj = {
+        email: enteredEmailRef.current.value,
+        password: enteredPaswordRef.current.value,
+        returnSecureToken: true,
+      };
+      const rseponse = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBfgANxrAz4GiGlY8rVOTnzXdsyhltW2vA",
+        {
+          method: "POST",
+          body: JSON.stringify(userobj),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await rseponse.json();
+      if (data.error) {
+        console.log(data.error.message);
+        alert(`${data.error.message}`, setloading(false));
+      }
+    } catch (error) {
+      console.log(error);
+
+      // error ? alert("try again getting error", setloading(false)) : " ";
+    }
   };
 
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form>
+      <form onSubmit={loginHandler}>
         <div className={classes.control}>
           <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required />
+          <input type="email" id="email" required ref={enteredEmailRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="password">Your Password</label>
-          <input type="password" id="password" required />
+          <input
+            type="password"
+            id="password"
+            required
+            ref={enteredPaswordRef}
+          />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? "Login" : "Create Account"}</button>
+          {loading ? (
+            <span className={classes.actspan}>Sending request....</span>
+          ) : (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
           <button
             type="button"
             className={classes.toggle}
